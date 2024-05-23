41df176f056dffc550f4b598853aa77ca9d1fd0a
Joan <joan.marinoso@estudiantat.upc.edu>
2024-05-23 23:16:40 +0200

    Exemple de components vue per mostrar la pagina principal i part de la informació dels threads fent una consulta a la api desplegada al heroku

---
+++ src/components/ShowThreads.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="thread">
+    <h2>{{ thread.title }}</h2>
+    <p>{{ thread.body }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShowThreads',
+  props: {
+    thread: Object,
+  },
+};
+</script>
+
+<style scoped>
+.thread {
+  border: 1px solid #ddd;
+  padding: 16px;
+  margin-bottom: 16px;
+}
+</style>
