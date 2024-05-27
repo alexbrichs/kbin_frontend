596abf7edc0c84e5568985f0a7f837e182a57b88
alex.brichs <alex.brichs@estudiantat.upc.edu>
2024-05-27 23:46:16 +0200

    T160-ObtenirComentaris-Publicacio

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import {createRouter, createWebHistory} from 'vue-router'
+import HomePage from "../components/HomePage.vue";
 import ShowDetallThread from "../components/ShowDetallThread.vue";
 
 const routes = [
     {
         path: '/',
-        name: 'ShowDetallThread',
-        component: ShowDetallThread
+        name: 'HomePage',
+        component: HomePage
     },
+    {
+        path: '/thread/:id/top/',
+        name: 'ShowDetallThread',
+        component: ShowDetallThread,
+        props: true
+    }
 ]
 
 const router = createRouter({
