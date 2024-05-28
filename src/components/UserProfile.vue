1d286b771ea040fe86d9071379d130e65f75ce84
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2024-05-28 23:23:31 +0200

    T-171-Inici veure les publicacions del perfil

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -95,6 +95,119 @@
             </div>
           </div>
         </div>
+
+        <aside class="options" id="options">
+          <div></div>
+          <menu class="options__main" style="align-items: center; justify-content: center;">
+            <li>
+              <router-link :to="`/u/${user.username}`" :class="{ active: selected === 'threads' }">
+                <span style="vertical-align: inherit;">threads ({{ user.total_threads }})</span>
+              </router-link>
+            </li>
+            <li>
+              <router-link :to="`/u/${user.username}/commented`" :class="{ active: selected === 'com' }">
+                <span style="vertical-align: inherit;">comments ({{ user.total_comments }})</span>
+              </router-link>
+            </li>
+            <li v-if="user.token !== null">
+              <router-link :to="`/u/${user.username}/boosts`" :class="{ active: selected === 'boost' }">
+                <span style="vertical-align: inherit;">boosts ({{ user.total_boosts }})</span>
+              </router-link>
+            </li>
+          </menu>
+        </aside>
+
+        <aside v-if="selected !== 'com'" class="options options--top" id="options"
+               style="align-items: center; justify-content: center;">
+          <div></div>
+          <menu class="options__main no-scroll">
+            <li>
+              <router-link :to="`/u/${user.username}/${selected !== 'threads' ? 'boosts/' : ''}top/${active_filter}`"
+                           :class="{ active: active_option === 'top' }">
+                <font style="vertical-align: inherit;">top</font>
+              </router-link>
+            </li>
+            <li>
+              <router-link
+                  :to="`/u/${user.username}/${selected !== 'threads' ? 'boosts/' : ''}newest/${active_filter}`"
+                  :class="{ active: active_option === 'newest' }">
+                <font style="vertical-align: inherit;">newest</font>
+              </router-link>
+            </li>
+            <li>
+              <router-link :to="`/u/${user.username}/${selected !== 'threads' ? 'boosts/' : ''}com/${active_filter}`"
+                           :class="{ active: active_option === 'com' }">
+                <font style="vertical-align: inherit;">commented</font>
+              </router-link>
+            </li>
+          </menu>
+          <menu class="options__filters">
+            <li class="dropdown">
+              <button aria-label="Filtrar per tipus" title="Filtrar per tipus"><i class="fa-solid fa-filter"></i> Filter
+                by type
+              </button>
+              <ul class="dropdown__menu">
+                <li>
+                  <router-link :to="`/u/${user.username}/${active_option}/tot`"
+                               :class="{ active: active_filter === 'tot' }">
+                    Tot
+                  </router-link>
+                </li>
+                <li>
+                  <router-link :to="`/u/${user.username}/${active_option}/links`"
+                               :class="{ active: active_filter === 'links' }">
+                    Links
+                  </router-link>
+                </li>
+                <li>
+                  <router-link :to="`/u/${user.username}/${active_option}/threads`"
+                               :class="{ active: active_filter === 'threads' }">
+                    Threads
+                  </router-link>
+                </li>
+              </ul>
+            </li>
+          </menu>
+        </aside>
+
+        <aside v-else class="options options--top" id="options" style="align-items: center; justify-content: center;">
+          <div></div>
+          <menu class="options__main no-scroll">
+            <li>
+              <router-link :to="`/u/${user.username}/commented/top/${active_filter}`"
+                           :class="{ active: active_option === 'top' }">
+                <font style="vertical-align: inherit;">top</font>
+              </router-link>
+            </li>
+            <li>
+              <router-link :to="`/u/${user.username}/commented/newest/${active_filter}`"
+                           :class="{ active: active_option === 'newest' }">
+                <font style="vertical-align: inherit;">newest</font>
+              </router-link>
+            </li>
+            <li>
+              <router-link :to="`/u/${user.username}/commented/oldest/${active_filter}`"
+                           :class="{ active: active_option === 'oldest' }">
+                <font style="vertical-align: inherit;">oldest</font>
+              </router-link>
+            </li>
+          </menu>
+        </aside>
+
+        <!--
+        <div id="content" class="overview subjects comments-tree comments show-comment-avatar show-post-avatar">
+          <template v-if="selected !== 'com'">
+            <div v-for="thread in user.data" :key="thread.id">
+              <ShowThreads :thread="thread"></ShowThreads>
+            </div>
+          </template>
+          <template v-else>
+            <div v-for="(comment, thread) in user.data" :key="comment.id">
+              <comment-component :comment="comment" :thread="thread"></comment-component>
+            </div>
+          </template>
+        </div>
+        -->
       </main>
     </div>
   </div>
@@ -112,7 +225,11 @@ export default {
   data() {
     return {
       user: {},
+      data: {},
       actiu: 'actiu',
+      selected: 'threads',
+      active_filter: 'tot',
+      active_option: 'newest'
     };
   },
   mounted() {
@@ -123,12 +240,13 @@ export default {
       try {
         // const apiKey = await this.getKey();
         const apiKey = localStorage.getItem('authToken');
-        const response = await axios.get(`https://bravo13-36a68ba47d34.herokuapp.com/api/u/${this.username}/`, {
+        const response = await axios.get(`https://bravo13-36a68ba47d34.herokuapp.com/api/u/${this.username}/threads/newest/tot/`, {
           headers: {
             'Authorization': `${apiKey}`
           }
         });
-        this.user = response.data;
+        this.user = response.data.user;
+        this.data = response.data.data
         console.log(this.user);
       } catch (error) {
         console.error('Error fetching user:', error);
