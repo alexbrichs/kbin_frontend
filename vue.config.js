fec0b11bfa58657b48b4a50514de4ee7df010a9c
Joan <joan.marinoso@estudiantat.upc.edu>
2024-05-31 02:48:19 +0200

    deploy heroku v7

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,6 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
   compress: true,
-  inline: true,
   allowedHosts: [
       'https://kbin-frontend-2c0c17ed7b02.herokuapp.com/'
   ]
