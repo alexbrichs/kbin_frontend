f1d4c5dc0e7dfda55e5b5a47a5afece169c7ab9b
Joan <joan.marinoso@estudiantat.upc.edu>
2024-05-31 04:21:30 +0200

    deploy heroku v26

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
-const { fileURLToPath } = require('url');
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Define dirname for ES modules
-const mainModulePath = require.resolve(process.argv[1]);
-const filename = fileURLToPath(mainModulePath);
-const dirname = dirname(filename);
-// Serve static files from the dist directory
-app.use(express.static(path.join(dirname, 'dist')));
+// Servir archivos estáticos desde la carpeta 'dist'
+app.use(express.static(path.join(__dirname, 'dist')));
 
-// Handle all routes by serving the index.html file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(dirname, 'dist', 'index.html'));
+// Manejar cualquier ruta y servir el archivo de entrada de la aplicación Vue.js
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Server is running on port ${PORT}");
+  console.log("Servidor escuchando en el puerto ${PORT}");
 });
