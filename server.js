de0a19a763e2d39f6e77b056de599a46387d6e23
Joan <joan.marinoso@estudiantat.upc.edu>
2024-05-31 03:59:28 +0200

    deploy heroku v21

--- server.js
+++ server.js
@@ -1,22 +1,7 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Define dirname for ES modules
-const filename = fileURLToPath(import.meta.url);
-const dirname = path.dirname(filename);
-
-// Serve static files from the dist directory
-app.use(express.static(path.join(dirname, 'dist')));
-
-// Handle all routes by serving the index.html file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(dirname, 'dist', 'index.html'));
-});
-
-app.listen(PORT, () => {
-  console.log("Server is running on port ${PORT}");
-});
+const express = require('express');
+const serveStatic = require("serve-static")
+const path = require('path');
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 3000;
+app.listen(port);
