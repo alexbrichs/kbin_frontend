f6c94f6e76bd28fc872a9a0c998bb1e7e164b497
Joan <joan.marinoso@estudiantat.upc.edu>
2024-05-31 03:02:12 +0200

    deploy heroku v12

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    compress: true,
-    disableHostCheck: true,   // That solved it
- }
+  transpileDependencies: true
 })
